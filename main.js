dfc4b7816ea2703eede6db9e4cc1d4e6c4f927f4
Robbie Kruszynski <robbiekruszynski@Robbies-MacBook-Pro-2.local>
2022-01-04 16:31:12 +0100

    bing bong smoke

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 const Discord = require('discord.js');
-const bdClient = new Discord.Client();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const fs = require('fs');
+// require('dotenv').config();
 
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
-bdClient.once('ready', () => {
-    console.log('Blockdaemon role bot is online');
+client.once('ready', () => {
+    console.log('Blockdaemon role bot is online, bing bong');
 });
 
+const prefix = '!';
+
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+       
+    if (command == "bing"){
+        message.channel.send('bong!');
+    }
+});
+
+
+// client.login(process.env.TOKEN);
+
+
+
 //keep at bottom
-bdClient.login('');
+client.login('OTI3OTIxODg3MDYzOTk0NDI5.YdRQmQ.WW6zEP93z8YpnurKU2bQObodXdc');
